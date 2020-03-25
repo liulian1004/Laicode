2e011c5c49767dc144f1cb50f0b3c6372605af15
Lian <liulian1004@hotmail.com>
2020-03-24 19:29:27 -0700

    update

--- 295-cousion-in-binary-tree/cousioninbinarytree.java
+++ 295-cousion-in-binary-tree/cousioninbinarytree.java
@@ -9,7 +9,7 @@
  * }
  */
 public class Solution {
-  public boolean isCousin(TreeNode root, int a, int b) {
+  public boolean isCousin(TreeNode root, TreeNode a, TreeNode b) {
     // T: O(n + list.length) S: O(list.length)
     // A: no duplicated elements
     // bst + map(node, root)
@@ -20,27 +20,64 @@ public class Solution {
     Deque<TreeNode> queue = new ArrayDeque<>();
     queue.add(root);
     while(!queue.isEmpty()) {
-      Map<Integer, Integer> map = new HashMap<>();
+      TreeNode pa = null;
+      TreeNode pb = null;
       int size = queue.size();
       for(int i = size; i > 0; i--) {
         TreeNode cur = queue.poll();
         if(cur.left != null) {
+          if(cur.left == a) {
+              pa = cur;
+          }else if(cur.left == b)  {
+            pb = cur;
+          }
           queue.offer(cur.left);
-          map.put(cur.left.key, cur.key);
         }
         if(cur.right != null) {
+          if(cur.right == a) {
+              pa = cur;
+          }else if(cur.right == b)  {
+            pb = cur;
+          }
           queue.offer(cur.right);
-          map.put(cur.right.key, cur.key);
         }
       }
-      if(map.containsKey(a) && map.containsKey(b)) {
-         int v1 = map.get(a);
-         int v2 = map.get(b);
-         if(v1 != v2) {
-              return true;
-          }
+      if(pa != null && pb != null) {
+        return pa  != pb;
+      } else if(pa != null || pb!= null) {
+        return false;
       }
+
     }
     return false;
   }
 }
+//another solution DFS
+private boolean isCousin(TreeNode root, int a, int b) {
+       List<Integer> list = new ArrayList<>();
+       if (root == null) {
+           return false;
+       }
+       helper(root, 0, null, a, b, list);
+       if(list.size() == 4 && list.get(0) == list.get(2)) {
+           return list.get(1) != list.get(3) ;
+       }
+       return false;
+  }
+  private void helper(TreeNode root, int level, TreeNode parent, int a, int b, List<Integer> list) {
+       if(root == null || list.size() == 4) {
+           return;
+       }
+
+       if(root.key == a) {
+           list.add(level);
+           list.add(parent.key);
+       }
+      if(root.key == b) {
+          list.add(level);
+          list.add(parent.key);
+      }
+       helper(root.left, level+1, root, a, b, list);
+       helper(root.right, level+1, root, a, b, list);
+
+  }
