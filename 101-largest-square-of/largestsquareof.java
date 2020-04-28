4d68675d5b8ce3323045d3360c628950c44be01c
Lian <liulian1004@hotmail.com>
2020-04-28 11:51:15 -0700

    update

--- 101-largest-square-of/largestsquareof.java
+++ 101-largest-square-of/largestsquareof.java
@@ -4,7 +4,7 @@ public class Solution {
     //m[i+1][j+1] represent the size of 1s square  with  i, j as the the  bottom right corner
     // T: n^2 , S n^2
     //my soultion
-    
+
     if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
       return 0;
     }
@@ -43,5 +43,29 @@ public class Solution {
       }
     }
     return res;
+  }
+  //优化空间
+  public int largest(int[][] matrix) {
+    // s: O(n) T: O(n * n)
+    //如果需要优化空间，那每一个dp[i%2][j]都需要更新值，对不符合条件的dp[i][j]重置0
+    if(matrix.length == 0 || matrix[0].length == 0) {
+      return 0;
+    }
+    int row = matrix.length;
+    int col = matrix[0].length;
+    int[][] dp = new int[2][col+1];
+    int ans = 0;
+    for(int i = 1; i <= row; i++) {
+      for(int j = 1; j <= col; j++) {
+
+        if(matrix[i-1][j-1] == 1) {
+          dp[i%2][j] = Math.min(dp[(i-1)%2][j-1], Math.min(dp[(i-1)%2][j],dp[i%2][j-1])) + 1;
+          ans = Math.max(dp[i%2][j], ans);
+        }else{
+          dp[i%2][j] = 0;
+        }
+      }
+    }
+    return ans;
   }
 }
--- 12-fibonacci-number/fibonaccinumber.java
+++ 12-fibonacci-number/fibonaccinumber.java
@@ -37,4 +37,26 @@ public class Solution {
     }
     return b;
   }
+  // recsurive
+  public long fibonacci(int k) {
+    // T: O(k) S: O(k)
+    if(k <= 0) {
+      return 0;
+    }
+    long[] a = new long[]{0};
+    long[] b = new long[]{1};
+    helper(k,a, b);
+    return b[0];
+  }
+  private void helper(int k, long[] a, long[] b) {
+    if(k == 1) {
+      return;
+    }
+    helper(k-1, a, b);
+    long temp = a[0] + b[0];
+    a[0] = b[0];
+    b[0] = temp;
+  }
+}
+
 }
