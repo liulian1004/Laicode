3ab6bd9d5846de1fee125e7735f88283051fedd5
Lian <liulian1004@hotmail.com>
2020-06-19 23:26:51 -0700

    update

--- 116-array-deduplication-ii/arraydeduplicationii.java
+++ 116-array-deduplication-ii/arraydeduplicationii.java
@@ -1,24 +1,15 @@
 public class Solution {
   public int[] dedup(int[] array) {
     // Write your solution here
-    if(array.length <= 1) {
-      return array;
-    }
-    int slow = 1;
-    //count原始状态是1，因为initialize的状态是已经默认第一个元素copy完成
-    int count = 1;
-    for(int fast = 1; fast < array.length; fast++) {
-      if(array[fast] != array[slow-1]) {
-        array[slow] =array[fast];
-        slow++;
-        count = 1;
-        //fast == slow -1
-        //coun <2 才能继续copy
-      } else if(count < 2) {
-          array[slow] = array[fast];
-          slow++;
-          count++;
+    if(array.length <= 2) {
+        return array;
       }
+      int i = 2;
+
+      for(int j = 2; j < array.length; j++) {
+        if(array[j] != array[i-1] || array[j] != array[i-2]) {
+          array[i++]= array[j];
+        }
     }
     return Arrays.copyOf(array, slow);
   }
