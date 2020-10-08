12985a557c225ec09958accd5340250838bdbfd0
Lian <liulian1004@hotmail.com>
2020-10-07 21:14:17 -0700

    update

--- 148-decode-ways/decodeways.java
+++ 148-decode-ways/decodeways.java
@@ -1,35 +1,26 @@
 public class Solution {
-  public int numDecodeWay(String input) {
+  public int numDecodeWay(String s) {
     //T: O(n) S: O(n)
     // case 1 input.charAt(i) != 0 ==> dp[i] += dp[i-1]
     // case 2 substring(i-1,i+1) is valid ==> dp[i] += dp[i-2]
-
-    if(input == null || input.length() == 0) {
-      return 0;
-    }
-
-    int[] dp = new int[input.length()];
-    //base case
-    if(Integer.parseInt(input.substring(0,1)) != 0) {
-      dp[0] = 1;
-    } else {
-      return 0;
-    }
-    for(int i = 1; i < input.length(); i++) {
-         if(Integer.parseInt(input.substring(i,i+1)) != 0) {
-            dp[i] += dp[i-1];
-          }
-          int digital = Integer.parseInt(input.substring(i-1,i+1));
-          if(digital >= 10 && digital <= 26) {
-            if(i == 1) {
-              dp[i] += 1;
-              continue;
+    int[] dp = new int[s.length()+1];
+            dp[0] = 1;
+            for(int index = 0; index < s.length(); index++){
+                 int one = (int)(s.charAt(index) - '0');
+                 if(one >= 1 && one <= 9){
+                     dp[index+1] += dp[index];
+                 }
+                 if(index >= 1){
+                     int prev = (int)(s.charAt(index-1) - '0');
+                     if(prev == 1 && one >= 0 && one <= 9
+                  || prev == 2 && one >= 0 && one <= 6) {
+                         dp[index+1] += dp[index-1];
+                     }
+                 }
             }
-            dp[i] += dp[i-2];
-          }
-    }
-    return dp[input.length() - 1];
-  }
+            return dp[s.length()];
+      }
+
   //follow up
   // if stirng contains * and * present 1 - 9
    1* = 19 ； * = 9 ； 2* = 9 + 6 = 15
